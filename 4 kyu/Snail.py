432f54be66c3514d4b127854fde9b0b251ad8b52
mynameisunavailable <90678707+mynameisunavailable@users.noreply.github.com>
2022-01-27 20:47:44 +0800

    Update and rename Snail to Snail.py

--- 4 kyu/Snail.py
+++ 4 kyu/Snail.py
@@ -1,24 +1,24 @@
-Snail Sort
-Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+# Snail Sort
+# Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
 
-array = [[1,2,3],
-         [4,5,6],
-         [7,8,9]]
-snail(array) #=> [1,2,3,6,9,8,7,4,5]
-For better understanding, please follow the numbers of the next array consecutively:
+# array = [[1,2,3],
+#          [4,5,6],
+#          [7,8,9]]
+# snail(array) #=> [1,2,3,6,9,8,7,4,5]
+# For better understanding, please follow the numbers of the next array consecutively:
 
-array = [[1,2,3],
-         [8,9,4],
-         [7,6,5]]
-snail(array) #=> [1,2,3,4,5,6,7,8,9]
-This image will illustrate things more clearly:
+# array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+# snail(array) #=> [1,2,3,4,5,6,7,8,9]
+# This image will illustrate things more clearly:
 
 
-NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+# NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
 
-NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
+# NOTE 2: The 0x0 (empty matrix) is represented as en empty array inside an array [[]].
 
-ALGORITHMS ARRAYS
+# ALGORITHMS ARRAYS
 
 
 Solution:
@@ -83,4 +83,3 @@ def snail(snail_map):
         solution.append(snail_map[path_history_v[i]][path_history_h[i]])
     
     return solution
-    
